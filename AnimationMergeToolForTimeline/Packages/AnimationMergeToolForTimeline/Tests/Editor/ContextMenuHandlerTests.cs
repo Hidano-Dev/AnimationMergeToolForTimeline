09da325f119ffe71bce738d634bb26fe8f8e7c6d
Hidano <n.hidano@hidano-dev.com>
2026-01-26 01:17:12 +0900

    task 8.2.4

--- AnimationMergeToolForTimeline/Packages/AnimationMergeToolForTimeline/Tests/Editor/ContextMenuHandlerTests.cs
+++ AnimationMergeToolForTimeline/Packages/AnimationMergeToolForTimeline/Tests/Editor/ContextMenuHandlerTests.cs
@@ -157,5 +157,161 @@ namespace AnimationMergeTool.Editor.Tests
         }
 
         #endregion
+
+        #region 複数選択対応 テスト (タスク8.2.4)
+
+        [Test]
+        public void GetSelectedTimelineAssets_複数のTimelineAssetを取得できる()
+        {
+            // Arrange
+            var timeline1 = ScriptableObject.CreateInstance<TimelineAsset>();
+            var timeline2 = ScriptableObject.CreateInstance<TimelineAsset>();
+            var timeline3 = ScriptableObject.CreateInstance<TimelineAsset>();
+
+            try
+            {
+                UnityEditor.Selection.objects = new Object[] { timeline1, timeline2, timeline3 };
+
+                // Act
+                var timelineAssets = ContextMenuHandler.GetSelectedTimelineAssets();
+
+                // Assert
+                Assert.AreEqual(3, timelineAssets.Length);
+            }
+            finally
+            {
+                UnityEditor.Selection.objects = new Object[0];
+                Object.DestroyImmediate(timeline1);
+                Object.DestroyImmediate(timeline2);
+                Object.DestroyImmediate(timeline3);
+            }
+        }
+
+        [Test]
+        public void GetSelectedTimelineAssets_TimelineAsset以外のオブジェクトは除外される()
+        {
+            // Arrange
+            var timeline = ScriptableObject.CreateInstance<TimelineAsset>();
+            var go = new GameObject("TestObject");
+
+            try
+            {
+                UnityEditor.Selection.objects = new Object[] { timeline, go };
+
+                // Act
+                var timelineAssets = ContextMenuHandler.GetSelectedTimelineAssets();
+
+                // Assert
+                Assert.AreEqual(1, timelineAssets.Length);
+                Assert.AreSame(timeline, timelineAssets[0]);
+            }
+            finally
+            {
+                UnityEditor.Selection.objects = new Object[0];
+                Object.DestroyImmediate(timeline);
+                Object.DestroyImmediate(go);
+            }
+        }
+
+        [Test]
+        public void GetSelectedPlayableDirectors_PlayableDirectorのないGameObjectは除外される()
+        {
+            // Arrange
+            var go1 = new GameObject("TestDirector");
+            var director = go1.AddComponent<PlayableDirector>();
+            var go2 = new GameObject("TestObject");
+
+            try
+            {
+                UnityEditor.Selection.gameObjects = new GameObject[] { go1, go2 };
+
+                // Act
+                var directors = ContextMenuHandler.GetSelectedPlayableDirectors();
+
+                // Assert
+                Assert.AreEqual(1, directors.Length);
+                Assert.AreSame(director, directors[0]);
+            }
+            finally
+            {
+                UnityEditor.Selection.objects = new Object[0];
+                Object.DestroyImmediate(go1);
+                Object.DestroyImmediate(go2);
+            }
+        }
+
+        [Test]
+        public void CanExecuteFromProject_複数のTimelineAssetが選択されている場合trueを返す()
+        {
+            // Arrange
+            var timeline1 = ScriptableObject.CreateInstance<TimelineAsset>();
+            var timeline2 = ScriptableObject.CreateInstance<TimelineAsset>();
+
+            try
+            {
+                UnityEditor.Selection.objects = new Object[] { timeline1, timeline2 };
+
+                // Act
+                var result = ContextMenuHandler.CanExecuteFromProject();
+
+                // Assert
+                Assert.IsTrue(result);
+            }
+            finally
+            {
+                UnityEditor.Selection.objects = new Object[0];
+                Object.DestroyImmediate(timeline1);
+                Object.DestroyImmediate(timeline2);
+            }
+        }
+
+        [Test]
+        public void CanExecuteFromHierarchy_複数のPlayableDirectorが選択されている場合trueを返す()
+        {
+            // Arrange
+            var go1 = new GameObject("TestDirector1");
+            var director1 = go1.AddComponent<PlayableDirector>();
+            var go2 = new GameObject("TestDirector2");
+            var director2 = go2.AddComponent<PlayableDirector>();
+
+            try
+            {
+                UnityEditor.Selection.objects = new Object[] { go1, go2 };
+
+                // Act
+                var result = ContextMenuHandler.CanExecuteFromHierarchy();
+
+                // Assert
+                Assert.IsTrue(result);
+            }
+            finally
+            {
+                UnityEditor.Selection.objects = new Object[0];
+                Object.DestroyImmediate(go1);
+                Object.DestroyImmediate(go2);
+            }
+        }
+
+        [Test]
+        public void ExecuteForTimelineAssets_nullの場合falseを返す()
+        {
+            // Act
+            var result = ContextMenuHandler.ExecuteForTimelineAssets(null);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ExecuteForTimelineAssets_空配列の場合falseを返す()
+        {
+            // Act
+            var result = ContextMenuHandler.ExecuteForTimelineAssets(new TimelineAsset[0]);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        #endregion
     }
 }
