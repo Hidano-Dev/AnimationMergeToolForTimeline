949b334fc3bfa59c70f9ee982737fb45cefbdb66
Hidano <n.hidano@hidano-dev.com>
2026-01-26 15:06:01 +0900

    task TASK-008

--- AnimationMergeToolForTimeline/Packages/AnimationMergeToolForTimeline/Scripts/Editor/Domain/ExtrapolationProcessor.cs
+++ AnimationMergeToolForTimeline/Packages/AnimationMergeToolForTimeline/Scripts/Editor/Domain/ExtrapolationProcessor.cs
@@ -339,8 +339,8 @@ namespace AnimationMergeTool.Editor.Domain
             var lastKey = curve.keys[curve.keys.Length - 1];
             var lastKeyValue = curve.Evaluate(lastKeyTime);
 
-            // 接線を取得（outTangent を使用）
-            var tangent = lastKey.outTangent;
+            // 接線を取得（inTangent を使用。outTangentは最後のキーでは設定されていないことが多い）
+            var tangent = lastKey.inTangent;
 
             // 時間差を計算してExtrapolate
             var timeDelta = time - endTime;
